0f1c4065c99d952bc4255dc14d6bd67709a54ae2
xdeng <dengxian0714@gmail.com>
2023-09-08 18:35:43 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+### How to Migrate Content from Flomo to Logseq
+
+1. Export an offline html file from flomo. It will automatically generate a folder which named by your username in flomo.
+
+   ![image-20230908182725527](/Users/nreal/Library/Application Support/typora-user-images/image-20230908182725527.png)
+
+2. Run this script in terminal or pycharm to extract the .md files, which are named with the corresponding date.(You need to replace the path of the index.html firstly)
+
+3. Put these generated markdown files into Logseq/journals in your disk.
+
+   ![image-20230908183052425](/Users/nreal/Library/Application Support/typora-user-images/image-20230908183052425.png)
+
+   ![image-20230908182826130](/Users/nreal/Library/Application Support/typora-user-images/image-20230908182826130.png)
+
+4. Put the 'file' folder which contains all the image files in your flomo to logseq/assets.
+
+5. That's it.
+
+6. Besides, if you use Mac, you can find this file, logseq, in your iCloud folder, and you can sync the content between different devices.
+
+   
---
+++ csv2md.py
@@ -0,0 +1,51 @@
+from bs4 import BeautifulSoup
+import os
+
+# 读取本地HTML文件
+with open('/Users/nreal/Downloads/flomo@俏丽小X-20230905/index.html', 'r', encoding='utf-8') as f:
+    html_content = f.read()
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+# 查找所有符合条件的div
+memos = soup.find_all('div', {'class': 'memo'})
+
+# 创建一个目录来保存Markdown文件
+os.makedirs('markdown_files', exist_ok=True)
+
+# 使用字典来存储每个日期的所有 memos
+date_memo_dict = {}
+
+for memo in memos:
+    # 获取时间并格式化为日期
+    time_div = memo.find('div', {'class': 'time'})
+    date_str = time_div.text.split(' ')[0] if time_div else 'Unknown Date'
+
+    # 获取内容
+    content_div = memo.find('div', {'class': 'content'})
+    content = content_div.text if content_div else ''
+
+    # 获取文件
+    files_div = memo.find('div', {'class': 'files'})
+    files_content = ''
+    if files_div:
+        img_tags = files_div.find_all('img')
+        for img in img_tags:
+            src = img.get('src', '')
+            files_content += f'![image.png](../assets/{src})\n'
+
+    # 合并内容和文件
+    full_content = content + '\n' + files_content
+
+    # 将内容添加到字典中
+    if date_str in date_memo_dict:
+        date_memo_dict[date_str].append(full_content)
+    else:
+        date_memo_dict[date_str] = [full_content]
+
+# 创建Markdown文件并写入内容
+for date_str, memos in date_memo_dict.items():
+    with open(f'markdown_files/{date_str}.md', 'w', encoding='utf-8') as f:
+        f.write('\n---\n'.join(memos))
+
+print("Markdown files have been created.")
